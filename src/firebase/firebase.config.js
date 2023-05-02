dbc58e6b9480acf9bc9dc48ba99439f9b3d5c15f
NusratParvin <nusrat.official@gmail.com>
2023-05-02 08:01:10 +0400

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6zHtw1BtPIODWsm4jAyCCowRwuRK4VuM",
+  authDomain: "findchef-af508.firebaseapp.com",
+  projectId: "findchef-af508",
+  storageBucket: "findchef-af508.appspot.com",
+  messagingSenderId: "299784456382",
+  appId: "1:299784456382:web:dd01ccfd60a27e4d1d76ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
