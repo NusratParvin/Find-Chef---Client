a85a2be92201fe0abd93bfde72ae43c037b0144a
NusratParvin <nusrat.official@gmail.com>
2023-05-02 11:46:36 +0400

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Alert } from "flowbite-react";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
+    <Alert color="info">Alert!</Alert>
+    <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
 
       </div>
       <h1>Vite + React</h1>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,13 +4,30 @@ import {
     RouterProvider,
   } from "react-router-dom";
 import Main from "../layouts/Main";
+import RecipeDetails from "../pages/RecipeDetails/RecipeDetails";
+import Recipes from "../pages/Recipes/Recipes";
+import RecipesLayout from "../layouts/RecipesLayout";
   
   
   
   const router = createBrowserRouter([
     {
-      path:'/',
-      element:<Main></Main>,
+      path: "/",
+      element: <RecipesLayout></RecipesLayout>,
+      children: [
+        {
+          path: "/",
+          element: <Recipes></Recipes>,
+        },
+        {
+          path: ":id",
+          element: (
+            // <PrivateRoute>
+              <RecipeDetails></RecipeDetails>
+            // </PrivateRoute>
+          ),
+        },
+      ],
     }
     ]);
   
